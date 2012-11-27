137f87a158f96d821ee345650dee524c7feb0742
Sucio Kastro <kastro@macmini.local>
2012-11-27 17:42:05 -0500

    Initial Commit

---
+++ Tally Counter/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Tally Counter
+//
+//  Created by Juan Castro on 27/11/12.
+//  Copyright (c) 2012 Jempe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Tally Counter/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Tally Counter
+//
+//  Created by Juan Castro on 27/11/12.
+//  Copyright (c) 2012 Jempe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
